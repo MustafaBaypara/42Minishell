3a0c5e6c3bb5bf59687ed459770f8519e34d144e
Mustafa Baypara <94145699+MustafaBaypara@users.noreply.github.com>
2024-09-12 16:21:15 +0300

    Update loop.c

--- srcs/loop.c
+++ srcs/loop.c
@@ -24,7 +24,7 @@ static int	line_reader(t_global *g)
 	g->command_line = check_malloc(ft_strtrim(tmp, " "));
 	free(tmp);
 	add_history(g->command_line);
-	if (ft_strncmp(g->command_line, "bitir", 5) == 0)
+	if (ft_strncmp(g->command_line, "exit", 5) == 0)
 		return (0);
 	return (1);
 }
