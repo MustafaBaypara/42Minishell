b976ed56f6b59a8057c784077d5aa9cb5d1eb28d
Mustafa Baypara <94145699+MustafaBaypara@users.noreply.github.com>
2024-10-07 16:56:16 +0300

    echo

--- srcs/builtin/echo.c
+++ srcs/builtin/echo.c
@@ -6,7 +6,7 @@
 /*   By: mbaypara <mbaypara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 16:46:39 by mbaypara          #+#    #+#             */
-/*   Updated: 2024/10/07 16:51:41 by mbaypara         ###   ########.fr       */
+/*   Updated: 2024/10/07 16:55:12 by mbaypara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,11 @@ static void	counter(size_t *i, size_t *j, int *nl, t_command *cmd)
 			(*j) = 1;
 			while (cmd->value[*i][*j] == 'n')
 				(*j)++;
+			if (cmd->value[*i][*j] == '\0')
+				(*nl) = 0;
+			else
+				break ;
+			(*i)++;
 		}
 	}
 }
@@ -33,5 +38,15 @@ int	echo(t_command *c, t_global *g)
 	size_t	j;
 	int		nline;
 
-	
+	counter(&i, &j, &nline, c);
+	if (c->value[i])
+		ft_putstr_fd(c->value[i++], c->fd[1]);
+	while (c->value[i])
+	{
+		ft_putstr_fd(" ", c->fd[1]);
+		ft_putstr_fd(c->value[i++], c->fd[1]);
+	}
+	if (nline)
+		ft_putstr_fd("\n", c->fd[1]);
+	return (g->error_no = 0, 1);
 }
