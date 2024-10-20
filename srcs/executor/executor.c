68738db47f471bf3f6c27e27abe47910579cdb72
Mustafa Baypara <musti.baypara@gmail.com>
2024-10-20 21:49:20 +0300

    ıvır zıvır

--- srcs/executor/executor.c
+++ srcs/executor/executor.c
@@ -6,7 +6,7 @@
 /*   By: mbaypara <mbaypara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 14:12:15 by mbaypara          #+#    #+#             */
-/*   Updated: 2024/10/20 19:12:28 by mbaypara         ###   ########.fr       */
+/*   Updated: 2024/10/20 21:48:19 by mbaypara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,54 +80,82 @@ static void	fd_config(t_global *g, t_command *cmd, int *i)
 
 static void	execute_it(t_command *cmd, t_global *g, int i, int num)
 {
-	if (!num)
-		cmd->pid = fork();
-	if (cmd->pid == -1)
-	{
-		perror("FORK");
-		g->error_no = 1;
-		error_program(ERROR_FORK, g->error_no);
-	}
-	else if (cmd->pid == 0)
-	{
-		catch_signal(2);
-		dup2(cmd->fd[1], STDOUT_FILENO);
-		if (cmd->fd[1] != STDOUT_FILENO)
-			close(cmd->fd[1]);
-		dup2(cmd->fd[0], STDIN_FILENO);
-		if (cmd->fd[0] != STDIN_FILENO)
-			close(cmd->fd[0]);
-		close_fds(cmd->next, i);
-		execve(cmd->cmdpath, cmd->value, g->the_env);
-		error_program(0, g->error_no);
-	}
-	catch_signal(3);
+    // Eğer num 0 ise (yani birden fazla komut varsa)
+    if (!num)
+        // Yeni bir süreç oluşturmak için fork() kullan
+        cmd->pid = fork();
+
+    // Eğer fork() başarısız olursa
+    if (cmd->pid == -1)
+    {
+        perror("FORK"); // Hata mesajı yazdır
+        g->error_no = 1; // Hata numarasını 1 olarak ayarla
+        error_program(ERROR_FORK, g->error_no); // Hata programını çağır
+    }
+    // Eğer çocuk süreçte isek
+    else if (cmd->pid == 0)
+    {
+        catch_signal(2); // Çocuk süreçte sinyal yakalama fonksiyonunu çağır
+
+        // Çıkış dosya tanımlayıcısını standart çıkışa yönlendir
+        dup2(cmd->fd[1], STDOUT_FILENO);
+        // Eğer çıkış dosya tanımlayıcısı standart çıkış değilse, kapat
+        if (cmd->fd[1] != STDOUT_FILENO)
+            close(cmd->fd[1]);
+
+        // Giriş dosya tanımlayıcısını standart girişe yönlendir
+        dup2(cmd->fd[0], STDIN_FILENO);
+        // Eğer giriş dosya tanımlayıcısı standart giriş değilse, kapat
+        if (cmd->fd[0] != STDIN_FILENO)
+            close(cmd->fd[0]);
+
+        // Diğer dosya tanımlayıcılarını kapat
+        close_fds(cmd->next, i);
+
+        // Komutu çalıştır
+        execve(cmd->cmdpath, cmd->value, g->the_env);
+
+        // Eğer execve başarısız olursa hata programını çağır
+        error_program(0, g->error_no);
+    }
+
+    // Ana süreçte sinyal yakalama fonksiyonunu çağır
+    catch_signal(3); // ctrl c de readline girdi promtunu temizler
 }
 
 void	run(t_global *g, t_command *cmd, int i, int num)
 {
-	if (num)
-	{
-		cmd->pid = fork();
-		if (cmd->pid == -1)
-		{
-			perror("FORK");
-			g->error_no = 1;
-			error_program(0, g->error_no);
-		}
-		else if (cmd->pid != 0)
-			return (catch_signal(3));
-		catch_signal(2);
-	}
-	if (!builtin_check(cmd, num))
-		return ;
-	else if (is_command_ok(cmd, g))
-		execute_it(cmd, g, i, num);
-	else
-	{
-		if (num)
-			error_program(0, g->error_no);
-	}
+    // Eğer num 0'dan farklıysa (yani birden fazla komut varsa)
+    if (num)
+    {
+        // Yeni bir süreç oluşturmak için fork() kullan
+        cmd->pid = fork();
+        // Eğer fork() başarısız olursa
+        if (cmd->pid == -1)
+        {
+            perror("FORK"); // Hata mesajı yazdır
+            g->error_no = 1; // Hata numarasını 1 olarak ayarla
+            error_program(0, g->error_no); // Hata programını çağır
+        }
+        // Eğer çocuk süreçte değilsek (ana süreçteysek)
+        else if (cmd->pid != 0)
+            return (catch_signal(3)); // Sinyal yakalama fonksiyonunu çağır ve fonksiyondan çık
+        catch_signal(2); // Çocuk süreçte sinyal yakalama fonksiyonunu çağır
+    }
+    // çocuk process işler 
+    
+    // Eğer komut bir yerleşik komut değilse
+    if (!builtin_check(cmd, num))
+        return ; // Fonksiyondan çık
+    // Eğer komut yerleşik bir komut ise ve komut geçerliyse
+    else if (is_command_ok(cmd, g)) // komutun yolunu bulup kontrol eder
+        execute_it(cmd, g, i, num); // Komutu çalıştır
+    else
+    {
+        // Eğer num 0'dan farklıysa (yani birden fazla komut varsa)
+        if (num)
+            error_program(0, g->error_no); // Hata programını çağır
+    }
 }
 
 void	executor(t_global *g, int i, int num)
@@ -144,7 +172,7 @@ void	executor(t_global *g, int i, int num)
     // Dosya tanımlayıcılarını yapılandır
     fd_config(g, cmd, &i);
 
-    // Eğer komut listesi varsa ve bir sonraki komut varsa, num'u 1 yap
+    // single multiple komutları kontrol eder
     if (cmd && cmd->next)
         num = 1;
 
--- srcs/executor/executor_utils.c
+++ srcs/executor/executor_utils.c
@@ -3,85 +3,123 @@
 /*                                                        :::      ::::::::   */
 /*   executor_utils.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abakirca <abakirca@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mbaypara <mbaypara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 16:35:13 by mbaypara          #+#    #+#             */
-/*   Updated: 2024/10/19 17:45:48 by abakirca         ###   ########.fr       */
+/*   Updated: 2024/10/20 21:46:25 by mbaypara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include <sys/stat.h>
 #include <unistd.h>
-
+#include <stdio.h>	
 static int	check_path(t_command *cmd, t_global *g, int *i)
 {
-	struct stat	buf;
+    struct stat	buf; // Dosya bilgilerini tutmak için stat yapısı
 
-	if (!check_slash(cmd->value[0]))
-		return (0);
-	if (stat(cmd->value[0], &buf) == 0)
-	{
-		if (S_ISDIR(buf.st_mode))
-			return (catch_error(cmd, 4, g), *i = 1, 0);
-		if (access(cmd->value[0], X_OK) != 0)
-			return (catch_error(cmd, 3, g), *i = 1, 0);
-		return (1);
-	}
-	else
-	{
-		catch_error(cmd, 2, g);
-		*i = 1;
-	}
-	return (0);
+    // Eğer komutun değeri bir slash (/) içeriyorsa
+    if (!check_slash(cmd->value[0]))
+        return (0); // 0 döndür
+    // Dosya bilgilerini almak için stat fonksiyonunu kullan
+    if (stat(cmd->value[0], &buf) == 0)
+    {
+        // Eğer dosya bir dizin ise
+        if (S_ISDIR(buf.st_mode))
+            return (catch_error(cmd, 4, g), *i = 1, 0); // Hata yakala, i'yi 1 yap ve 0 döndür
+
+        // Eğer dosya çalıştırılabilir değilse
+        if (access(cmd->value[0], X_OK) != 0)
+            return (catch_error(cmd, 3, g), *i = 1, 0); // Hata yakala, i'yi 1 yap ve 0 döndür
+
+        return (1); // Dosya geçerli, 1 döndür
+    }
+    else
+    {
+        catch_error(cmd, 2, g); // Hata yakala
+        *i = 1; // i'yi 1 yap
+    }
+    return (0); // 0 döndür
 }
 
 static int	check_dir(char *path, t_global *g, t_command *cmd)
 {
-	struct stat	buf;
+    struct stat	buf; // Dosya bilgilerini tutmak için stat yapısı
 
-	if (stat(path, &buf) == 0)
-		if (S_ISDIR(buf.st_mode))
-			return (catch_error(cmd, 1, g), 1);
-	return (0);
+    // Dosya bilgilerini almak için stat fonksiyonunu kullan
+    if (stat(path, &buf) == 0)
+    {
+        // Eğer dosya bir dizin ise
+        if (S_ISDIR(buf.st_mode))
+        {
+            // Hata yakala ve 1 döndür
+            return (catch_error(cmd, 1, g), 1);
+        }
+    }
+
+    // Eğer dosya bir dizin değilse veya stat fonksiyonu başarısız olursa 0 döndür
+    return (0);
 }
 
 static int	path_command(t_command *cmd, t_global *g)
 {
-	char	*tmp;
-	size_t	i;
+    char	*tmp; // Geçici string değişkeni
+    size_t	i;    // Döngü için indeks değişkeni
 
-	i = 0;
-	tmp = check_malloc(ft_strjoin("/", cmd->value[0]));
-	while (g->path[i])
-	{
-		cmd->cmdpath = check_malloc(ft_strjoin(g->path[i], tmp));
-		if (check_dir(cmd->cmdpath, g, cmd))
-			return (cmd->cmdpath = NULL, 0);
-		if (access(cmd->cmdpath, X_OK) == 0)
-			return (1);
-		remove_from_list(&g->garbage_list, cmd->cmdpath);
-		cmd->cmdpath = NULL;
-		i++;
-	}
-	return (catch_error(cmd, 1, g), 0);
+    i = 0; // İndeksi sıfırla
+    // Komutun değerini bir slash (/) ile birleştir ve tmp'ye ata
+    tmp = check_malloc(ft_strjoin("/", cmd->value[0]));
+    // Path dizisi boyunca döngü
+    while (g->path[i])
+    {
+        // Path'in mevcut elemanını tmp ile birleştir ve cmd->cmdpath'e ata
+        cmd->cmdpath = check_malloc(ft_strjoin(g->path[i], tmp));
+        // Eğer dizin kontrolü başarılı olursa
+        if (check_dir(cmd->cmdpath, g, cmd)) // birebir aynı olması için ypaılmış X11 çalıştığında usr/bin/X11 bulursa burdan çıkıyor
+            // cmd->cmdpath'i NULL yap ve 0 döndür
+            return (cmd->cmdpath = NULL, 0);
+
+        // Eğer dosya çalıştırılabilir ise
+        if (access(cmd->cmdpath, X_OK) == 0) // cat yazıldığında bin/cat içinde buluyor ve çalışıp çalışmadığını kontrol ediyor
+            return (1); // 1 döndür
+
+        // EĞER YUKARDAKİLER OLMADIYSA YOKTUR DEMEK VE CMD KAPATILIYOR VE Garbage listeden cmd->cmdpath'i kaldır
+        remove_from_list(&g->garbage_list, cmd->cmdpath); // 
+
+        // cmd->cmdpath'i NULL yap
+        cmd->cmdpath = NULL;
+
+        // İndeksi artır
+        i++;
+    }
+
+    // Hata yakala ve 0 döndür
+    return (catch_error(cmd, 1, g), 0); // KOMUT HİÇ BİR YERDE YOKSA hatayı verir ve 0 döndürür
 }
 
 int	is_command_ok(t_command *cmd, t_global *g)
 {
-	int	i;
+    int	i; // Döngü ve kontrol için indeks değişkeni
 
-	i = 0;
-	if (cmd && (!cmd->value[0] || !cmd->value[0][0]))
-		return (0);
-	if (check_path(cmd, g, &i))
-	{
-		cmd->cmdpath = check_malloc(ft_strdup(cmd->value[0]));
-		return (1);
-	}
-	else if (i == 0 && path_command(cmd, g))
-		return (1);
-	return (0);
+    i = 0; // İndeksi sıfırla
+
+    // Eğer komut geçerli değilse veya komutun değeri boşsa, 0 döndür
+    if (cmd && (!cmd->value[0] || !cmd->value[0][0]))
+        return (0);
+
+    // sadece o dizindeki ./a.out gibi komutları kontrol eder
+    if (check_path(cmd, g, &i))
+    {
+        // Komutun path'ini kopyala ve cmd->cmdpath'e ata
+        cmd->cmdpath = check_malloc(ft_strdup(cmd->value[0]));
+        return (1); // Komut geçerli, 1 döndür
+    }
+    // Eğer path kontrolü başarısız olursa ve i 0 ise
+    else if (i == 0 && path_command(cmd, g))
+        return (1); // Komut geçerli, 1 döndür
+
+    // Komut geçerli değilse, 0 döndür
+    return (0);
 }
 
 void	close_fds(t_command *cmd, int i)
