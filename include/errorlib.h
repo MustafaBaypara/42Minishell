d64c0ae379d0d5138c1e194b2bec87f33e153bdb
Mustafa Baypara <musti.baypara@gmail.com>
2024-10-21 17:57:18 +0300

    last

--- include/errorlib.h
+++ include/errorlib.h
@@ -6,16 +6,16 @@
 /*   By: mbaypara <mbaypara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/09 15:24:23 by mbaypara          #+#    #+#             */
-/*   Updated: 2024/09/27 14:50:48 by mbaypara         ###   ########.fr       */
+/*   Updated: 2024/10/21 17:46:29 by mbaypara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ERRORLIB_H
 # define ERRORLIB_H
 
-# define ERROR_MALLOC "Error: Cannot allocate memory.\n"
-# define ERROR_ARG "ERROR: The program does not use any arguments!\n"
-# define ERROR_FORK "ERROR: Fork failed!\n"
-# define ERROR_PIPE "ERROR: Pipe failed!\n"
+# define ERROR_MALLOC "Error: Cannot allocate memory."
+# define ERROR_ARG "ERROR: The program does not use any arguments!"
+# define ERROR_FORK "ERROR: Fork failed!"
+# define ERROR_PIPE "ERROR: Pipe failed!"
 
 #endif
